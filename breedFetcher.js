67d625de8f46740b7434ca3974b3017bdc7d108c
charuzain <charuabhizain@gmail.com>
2021-11-10 11:54:43 -0500

    Initial commit

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,17 @@
-const breedName = process.argv.slice(2)
+const breedName = process.argv.slice(2);
 const request = require('request');
 request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
-  console.log('error:', error);
-  console.log('statusCode:', response && response.statusCode);
-  const data = JSON.parse(body);
-  if (data[0] === undefined) {
-    console.log("Breed not found")
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      console.log("Breed not found");
+    } else {
+      console.log(data[0].description);
+    }
   }
-  else {
-    console.log(data);
-  }
-
-
 });
+
 
 
